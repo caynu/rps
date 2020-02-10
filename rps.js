e2076a6cfe84213b23910f0717548e5fe671fb48
caynu <carljvanzwieten@gmail.com>
2020-02-10 14:30:14 +0800

    Need to add loops

--- rps.js
+++ rps.js
@@ -1,18 +1,21 @@
 console.log("Rock, Paper, Scissors!");
 
 var computerPlay = Math.floor(Math.random() * 3);
-var playerSelection = readline();
+var playerSelection = prompt("Enter your choice!");
 
 var playerScore = 0;
 var computerScore = 0;
 
-function addScore() {
+var player;
+var computer;
+
+
     if (player = true) {
         playerScore++;
     } else if (computer = true) {
-        computerScore++
+        computerScore++;
     }
-}
+
 
 function computerSelection () {
 }  if (computerPlay === 1) {
@@ -24,23 +27,32 @@ function computerSelection () {
 }
 
 function playRound(playerSelection,computerPlay){
-}  if (playerSelection === computerPlay) {
+    playerSelection = playerSelection.toLowerCase();
+    computerPlay = computerPlay.toLowerCase();
+  if (playerSelection === computerPlay) {
     console.log("It\'s a draw!");
     console.log (`Your score is: ${playScore}, while your oponent\s score is: ${computerScore}`);
 }  else if ((playerSelection === "rock") && (computerPlay === "scissors")) {
     console.log("Rock beats scissors! You won!");
-
+    player = true;
     console.log (`Your score is: ${playScore}, while your oponent\s score is: ${computerScore}`);
 }  else if ((playerSelection === "paper") && (computerPlay === "scissors")) {
     console.log("Scissors beat paper. You lose!");
-
+    computer = true;
     console.log (`Your score is: ${playScore}, while your oponent\s score is: ${computerScore}`);
 }  else if ((playerSelection === "rock") && (computerPlay === "paper")) {
     console.log("Paper beats rock! You lose!");
-
+    computer = true;
     console.log (`Your score is: ${playScore}, while your oponent\s score is: ${computerScore}`);
 }  else if ((playerSelection === "paper") && (computerPlay === "rock")) {
     console.log("Paper beats rock! You won!");
-
+    player = true;
     console.log (`Your score is: ${playScore}, while your oponent\s score is: ${computerScore}`)
 } 
+
+  if (playerScore === 3) {
+    console.log("You won!");
+}  else if (computerScore === 3) {
+    console.log("Computer won!");
+  }
+}
