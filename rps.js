47a6ab81e475de2f914a576dc5d7f5a381a827ed
caynu <carljvanzwieten@gmail.com>
2020-02-10 12:47:21 +0800

    Started the project

---
+++ rps.js
@@ -0,0 +1,13 @@
+console.log("Rock, Paper, Scissors!")
+
+var computer = Math.floor(Math.random() * 3);
+var playerSelection = readline();
+
+function computerSelection (computer) {
+}  if (computer = 1) {
+    computer = "rock";
+}  else if (computer = 2) {
+    computer = "paper";
+}  else {
+    computer = "scissors";
+}
